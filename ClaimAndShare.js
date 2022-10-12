c71f21ac28b2ea5fbb82059dae9559ccb86b6763
0xToshi <jsonpayment@gmail.com>
2022-10-12 23:01:43 +0700

    Claim And Send
    Cheaper Gas

---
+++ ClaimAndShare.js
@@ -0,0 +1,27 @@
+const { config } = require('./Config');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(config.RPC));
+const ABI = require('./ABI/minter.abi.json');
+//const WalletList = require("./wallet.json");
+const { sendTx } = require("./lib/SendTx");
+
+(async() => {
+
+    const Xen = new web3.eth.Contract(ABI, config.ContractAddress);
+    let TxData = await Xen.methods.claimMintRewardAndShare(config.mainWallet, 100).encodeABI();
+    console.log(TxData);
+    let BroadcastTx = [];
+    
+    for(wallet of WalletList)
+    {
+        BroadcastTx.push(sendTx(wallet,
+            config.Gwei,
+            "120000",
+            config.ContractAddress,
+            TxData,
+            "0" 
+        ))
+    }
+    Promise.all( BroadcastTx )
+    
+})()
